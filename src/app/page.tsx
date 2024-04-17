2afae3375469232f6f4fe3f4e4c519cd1f703dc0
Raye <126174767+Dempserayo@users.noreply.github.com>
2024-04-17 14:08:24 -0500

    Feat<Navbar,Hero>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,88 @@
+import React from "react";
 import Image from "next/image";
+import { AiOutlineGlobal } from "react-icons/ai";
+import { FiMoreHorizontal } from "react-icons/fi";
+import { FaPlay } from "react-icons/fa6";
+import { SlArrowRight } from "react-icons/sl";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <nav className="flex flex-row  items-center justify-center gap-40 shadow-lg max-[1000px]:hidden">
+        <ul className="flex flex-row justify-center item-center gap-10 p-4 text-sm text-white ">
+          <Image
+            src="imagenes\logo\Quickit.svg"
+            alt="Logo Quickit"
+            width={100}
+            height={100}
+            className="mx-8"
+          />
+
+          <li className="font-bold flex items-center  transition ease-in-out delay-150 hover:-translate-y- hover:scale-110  duration-300">
+            <button>Why Quickit</button>
+          </li>
+          <li className="font-bold flex items-center  transition ease-in-out delay-150 hover:-translate-y- hover:scale-110  duration-300">
+            <button>Solutions</button>
+          </li>
+          <li className="font-bold flex items-center  transition ease-in-out delay-150 hover:-translate-y- hover:scale-110  duration-300">
+            <button>Resources</button>
+          </li>
+          <li className="font-bold flex items-center  transition ease-in-out delay-150 hover:-translate-y- hover:scale-110  duration-300">
+            <button>Pricing</button>
+          </li>
+        </ul>
+
+        <div className="flex justify-center gap-6 text-white">
+          <button className="font-bold flex items-center  transition ease-in-out delay-150 hover:-translate-y- hover:scale-110  duration-300">
+            <AiOutlineGlobal />
+          </button>
+          <span className="flex items-center"> | </span>
+          <button className="font-bold flex text-center text-sm items-center  transition ease-in-out delay-150 hover:-translate-y- hover:scale-110  duration-300">
+            Log In
+          </button>
+          <button className="bg-blue-500 w-36 h-12  font-bold flex text-center text-sm justify-center items-center transition ease-in-out delay-150 hover:-translate-y- hover:scale-110 hover:bg-indigo-500 duration-300">
+            Request a Demo
+          </button>
         </div>
-      </div>
+      </nav>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div className="flex flex-row justify-center max-[1000px]:hidden">
+        <div className="w-full p-20 mx-8">
+          <h1 className="text-white text-7xl z-50 font-extrabold flex flex-row justify-start w-full">
+            {" "}
+            Welcome to the
+          </h1>
+
+          <p className="font-extrabold text-7xl -z-50  text-transparent w-full h-28 bg-gradient-to-tr from-blue-500 to-cyan-300 bg-clip-text">
+            Quick Landings
+          </p>
+
+          <p className="text-white text-sm py-4 z-50">
+            {" "}
+            Create screens right in your Figma. Connect different blocks and
+            create <br /> awesome landings. Yes, it just takes a few minutes!
+          </p>
+
+          <button className="text-white font-bold bg-blue-500 w-40 h-14 mt-4 flex justify-center items-center gap-2 shadow-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+            Get Access
+            <p className="bg-blue-400 shadow-2xl bg-opacity-30 w-6 h-6 flex items-center justify-center text-xs">
+              <SlArrowRight />
+            </p>
+          </button>
+        </div>
+        <div className=" animate-pulse animate-ease-in-out -z-50  animate-alternate-reverse animate-fill-both">
+          <div className="bg-blue-500 bg-opacity-25   h-80 w-80 z-0  rounded-full flex justify-end blur-3xl absolute bottom-40 left-0"></div>
+          <div className="bg-white bg-opacity-25 h-96 w-96 z-0 rounded-full flex justify-end blur-3xl absolute bottom-40 right-0"></div>
+        </div>
+
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          src="imagenes\iPhone 12 _ 12 Pro.svg"
+          alt="phone"
+          width={200}
+          height={10}
+          className="mx-36 mt-8 "
+        ></Image>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
